d202c988f66257ac75361f791375c5eeb86f0953
kuldeep-tricon <kuldeep@triconinfotech.com>
2018-07-04 17:57:06 +0530

    updated

---
+++ neo4jdemo/src/main/java/com/example/neo4jdemo/controller/MyController.java
@@ -0,0 +1,10 @@
+package com.example.neo4jdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+
+}
